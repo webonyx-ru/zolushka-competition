96dcfc6c350d68290dc6ca9bd5704055c95797d5
sharipkanov <sharipkanov@gmail.com>
2017-06-27 18:14:06 +0600

    initial commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,244 +1,270 @@
-(function() {
+'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var YOURAPPNAME = function () {
     function YOURAPPNAME(doc) {
-        var _self = this;
+        _classCallCheck(this, YOURAPPNAME);
 
-        _self.doc = doc;
-        _self.window = window;
-        _self.html = _self.doc.querySelector('html');
-        _self.body= _self.doc.body;
-        _self.location = location;
-        _self.hash = location.hash;
-        _self.Object = Object;
-        _self.scrollWidth = 0;
+        this.doc = doc;
+        this.window = window;
+        this.html = this.doc.querySelector('html');
+        this.body = this.doc.body;
+        this.location = location;
+        this.hash = location.hash;
+        this.Object = Object;
+        this.scrollWidth = 0;
 
-        _self.bootstrap();
+        this.scrollWidth = this.scrollBarWidth();
     }
 
-    YOURAPPNAME.prototype.bootstrap = function() {
-        var _self = this;
-
-        // Initialize window scollBar width
-        _self.scrollWidth = _self.scrollBarWidth();
-    };
-
     // Window load types (loading, dom, full)
-    YOURAPPNAME.prototype.appLoad  = function (type, callback) {
-        var _self = this;
 
-        switch(type) {
-            case 'loading':
-                if (_self.doc.readyState === 'loading') callback();
 
-                break;
-            case 'dom':
-                _self.doc.onreadystatechange = function () {
-                    if (_self.doc.readyState === 'complete') callback();
-                };
+    _createClass(YOURAPPNAME, [{
+        key: 'appLoad',
+        value: function appLoad(type, callback) {
+            var _self = this;
 
-                break;
-            case 'full':
-                _self.window.onload = function(e) {
-                    callback(e);
-                };
+            switch (type) {
+                case 'loading':
+                    if (_self.doc.readyState === 'loading') callback();
 
-                break;
-            default:
-                callback();
+                    break;
+                case 'dom':
+                    _self.doc.onreadystatechange = function () {
+                        if (_self.doc.readyState === 'complete') callback();
+                    };
+
+                    break;
+                case 'full':
+                    _self.window.onload = function (e) {
+                        callback(e);
+                    };
+
+                    break;
+                default:
+                    callback();
+            }
         }
-    };
+    }, {
+        key: 'scrollBarWidth',
 
-    // Detect scroll default scrollBar width (return a number)
-    YOURAPPNAME.prototype.scrollBarWidth = function() {
-        var _self = this,
-            outer = _self.doc.createElement("div");
+
+        // Detect scroll default scrollBar width (return a number)
+        value: function scrollBarWidth() {
+            var _self = this;
+            var outer = _self.doc.createElement("div");
+
             outer.style.visibility = "hidden";
             outer.style.width = "100px";
             outer.style.msOverflowStyle = "scrollbar";
 
-        _self.body.appendChild(outer);
+            _self.body.appendChild(outer);
 
-        var widthNoScroll = outer.offsetWidth;
+            var widthNoScroll = outer.offsetWidth;
 
-        outer.style.overflow = "scroll";
+            outer.style.overflow = "scroll";
 
-        var inner = _self.doc.createElement("div");
+            var inner = _self.doc.createElement("div");
 
-        inner.style.width = "100%";
-        outer.appendChild(inner);
+            inner.style.width = "100%";
+            outer.appendChild(inner);
 
-        var widthWithScroll = inner.offsetWidth;
+            var widthWithScroll = inner.offsetWidth;
 
-        outer.parentNode.removeChild(outer);
+            outer.parentNode.removeChild(outer);
 
-        return widthNoScroll - widthWithScroll;
-    };
+            return widthNoScroll - widthWithScroll;
+        }
+    }, {
+        key: 'initSwitcher',
+        value: function initSwitcher() {
+            var _self = this;
 
-    YOURAPPNAME.prototype.initSwitcher = function () {
-        var _self = this;
+            var switchers = _self.doc.querySelectorAll('[data-switcher]');
 
-        var switchers = _self.doc.querySelectorAll('[data-switcher]');
+            if (switchers && switchers.length > 0) {
+                for (var i = 0; i < switchers.length; i++) {
+                    var switcher = switchers[i],
+                        switcherOptions = _self.options(switcher.dataset["switcher"]),
+                        switcherElems = switcher.children,
+                        switcherTargets = _self.doc.querySelector('[data-switcher-target="' + switcherOptions.target + '"]').children,
+                        switchersActive = [];
 
-        if(switchers && switchers.length > 0) {
-            for(var i=0; i<switchers.length; i++) {
-                var switcher = switchers[i],
-                    switcherOptions = _self.options(switcher.dataset.switcher),
-                    switcherElems = switcher.children,
-                    switcherTargets = _self.doc.querySelector('[data-switcher-target="' + switcherOptions.target + '"]').children;
+                    for (var y = 0; y < switcherElems.length; y++) {
+                        var switcherElem = switcherElems[y],
+                            parentNode = switcher.children,
+                            switcherTrigger = switcherElem.children.length ? switcherElem.children[0] : switcherElem,
+                            switcherTarget = switcherTargets[y];
 
-                for(var y=0; y<switcherElems.length; y++) {
-                    var switcherElem = switcherElems[y],
-                        parentNode = switcher.children,
-                        switcherTarget = switcherTargets[y];
+                        if (switcherElem.classList.contains('active')) {
+                            for (var z = 0; z < parentNode.length; z++) {
+                                parentNode[z].classList.remove('active');
+                                switcherTargets[z].classList.remove('active');
+                            }
+                            switcherElem.classList.add('active');
+                            switcherTarget.classList.add('active');
+                        } else switchersActive.push(0);
 
-                    if(switcherElem.classList.contains('active')) {
-                        for(var z=0; z<parentNode.length; z++) {
-                            parentNode[z].classList.remove('active');
-                            switcherTargets[z].classList.remove('active');
-                        }
-                        switcherElem.classList.add('active');
-                        switcherTarget.classList.add('active');
+                        switcherTrigger.addEventListener('click', function (elem, target, parent, targets) {
+                            return function (e) {
+                                e.preventDefault();
+
+                                if (!elem.classList.contains('active')) {
+                                    for (var _z = 0; _z < elem.parentNode.children.length; _z++) {
+                                        elem.parentNode.children[_z].classList.remove('active');
+                                        targets[_z].classList.remove('active');
+                                    }
+                                    elem.classList.add('active');
+                                    target.classList.add('active');
+                                }
+                            };
+                        }(switcherElem, switcherTarget, parentNode, switcherTargets));
                     }
 
-                    switcherElem.children[0].addEventListener('click', function (elem, target, parent, targets) {
-                        return function (e) {
-                            e.preventDefault();
-                            if(!elem.classList.contains('active')) {
-                                for(var z=0; z<parentNode.length; z++) {
-                                    parent[z].classList.remove('active');
-                                    targets[z].classList.remove('active');
-                                }
-                                elem.classList.add('active');
-                                target.classList.add('active');
-                            }
-                        };
-
-                    }(switcherElem, switcherTarget, parentNode, switcherTargets));
+                    if (switchersActive.length === switcherElems.length) {
+                        switcherElems[0].classList.add('active');
+                        switcherTargets[0].classList.add('active');
+                    }
                 }
             }
         }
-    };
-
-    YOURAPPNAME.prototype.str2json = function(str, notevil) {
-        try {
-            if (notevil) {
-                return JSON.parse(str
-                    .replace(/([\$\w]+)\s*:/g, function(_, $1){return '"'+$1+'":';})
-                    .replace(/'([^']+)'/g, function(_, $1){return '"'+$1+'"';})
-                );
-            } else {
-                return (new Function("", "var json = " + str + "; return JSON.parse(JSON.stringify(json));"))();
-            }
-        } catch(e) { return false; }
-    };
-
-    YOURAPPNAME.prototype.options = function(string) {
-        var _self = this;
-
-        if (typeof string !='string') return string;
-
-        if (string.indexOf(':') != -1 && string.trim().substr(-1) != '}') {
-            string = '{'+string+'}';
-        }
-
-        var start = (string ? string.indexOf("{") : -1), options = {};
-
-        if (start != -1) {
+    }, {
+        key: 'str2json',
+        value: function str2json(str, notevil) {
             try {
-                options = _self.str2json(string.substr(start));
-            } catch (e) {}
-        }
-
-        return options;
-    };
-
-    YOURAPPNAME.prototype.popups = function (options) {
-        var _self = this;
-
-        var defaults = {
-            reachElementClass: '.js-popup',
-            closePopupClass: '.js-close-popup',
-            currentElementClass: '.js-open-popup',
-            changePopupClass: '.js-change-popup'
-        };
-
-        options = $.extend({}, options, defaults);
-
-        var plugin = {
-            reachPopups: $(options.reachElementClass),
-            bodyEl: $('body'),
-            topPanelEl: $('.top-panel-wrapper'),
-            htmlEl: $('html'),
-            closePopupEl: $(options.closePopupClass),
-            openPopupEl: $(options.currentElementClass),
-            changePopupEl: $(options.changePopupClass),
-            bodyPos: 0
-        };
-
-        plugin.openPopup = function (popupName) {
-            plugin.reachPopups.filter('[data-popup="' + popupName + '"]').addClass('opened');
-            plugin.bodyEl.css('overflow-y', 'scroll');
-            plugin.topPanelEl.css('padding-right', scrollSettings.width);
-            plugin.htmlEl.addClass('popup-opened');
-        };
-
-        plugin.closePopup = function (popupName) {
-            plugin.reachPopups.filter('[data-popup="' + popupName + '"]').removeClass('opened');
-            setTimeout(function () {
-                plugin.bodyEl.removeAttr('style');
-                plugin.htmlEl.removeClass('popup-opened');
-                plugin.topPanelEl.removeAttr('style');
-            }, 500);
-        };
-
-        plugin.changePopup = function (closingPopup, openingPopup) {
-            plugin.reachPopups.filter('[data-popup="' + closingPopup + '"]').removeClass('opened');
-            plugin.reachPopups.filter('[data-popup="' + openingPopup + '"]').addClass('opened');
-        };
-
-        plugin.init = function () {
-            plugin.bindings();
-        };
-
-        plugin.bindings = function () {
-            plugin.openPopupEl.on('click', function (e) {
-                e.preventDefault();
-                var pop = $(this).attr('data-open-popup');
-                plugin.openPopup(pop);
-            });
-
-            plugin.closePopupEl.on('click', function (e) {
-                var pop;
-                if (this.hasAttribute('data-close-popup')) {
-                    pop = $(this).attr('data-close-popup');
+                if (notevil) {
+                    return JSON.parse(str.replace(/([\$\w]+)\s*:/g, function (_, $1) {
+                        return '"' + $1 + '":';
+                    }).replace(/'([^']+)'/g, function (_, $1) {
+                        return '"' + $1 + '"';
+                    }));
                 } else {
-                    pop = $(this).closest(options.reachElementClass).attr('data-popup');
+                    return new Function("", "const json = " + str + "; return JSON.parse(JSON.stringify(json));")();
                 }
+            } catch (e) {
+                return false;
+            }
+        }
+    }, {
+        key: 'options',
+        value: function options(string) {
+            var _self = this;
 
-                plugin.closePopup(pop);
-            });
+            if (typeof string !== 'string') return string;
 
-            plugin.changePopupEl.on('click', function (e) {
-                var closingPop = $(this).attr('data-closing-popup');
-                var openingPop = $(this).attr('data-opening-popup');
+            if (string.indexOf(':') !== -1 && string.trim().substr(-1) !== '}') {
+                string = '{' + string + '}';
+            }
 
-                plugin.changePopup(closingPop, openingPop);
-            });
+            var start = string ? string.indexOf("{") : -1;
+            var options = {};
 
-            plugin.reachPopups.on('click', function (e) {
-                var target = $(e.target);
-                var className = options.reachElementClass.replace('.', '');
-                if (target.hasClass(className)) {
-                    plugin.closePopup($(e.target).attr('data-popup'));
-                }
-            });
-        };
+            if (start !== -1) {
+                try {
+                    options = _self.str2json(string.substr(start));
+                } catch (e) {}
+            }
 
-        if (options)
-            plugin.init();
+            return options;
+        }
+    }, {
+        key: 'popups',
+        value: function popups(options) {
+            var _self = this;
 
-        return plugin;
-    };
+            var defaults = {
+                reachElementClass: '.js-popup',
+                closePopupClass: '.js-close-popup',
+                currentElementClass: '.js-open-popup',
+                changePopupClass: '.js-change-popup'
+            };
+
+            options = $.extend({}, options, defaults);
+
+            var plugin = {
+                reachPopups: $(options.reachElementClass),
+                bodyEl: $('body'),
+                topPanelEl: $('.top-panel-wrapper'),
+                htmlEl: $('html'),
+                closePopupEl: $(options.closePopupClass),
+                openPopupEl: $(options.currentElementClass),
+                changePopupEl: $(options.changePopupClass),
+                bodyPos: 0
+            };
+
+            plugin.openPopup = function (popupName) {
+                plugin.reachPopups.filter('[data-popup="' + popupName + '"]').addClass('opened');
+                plugin.bodyEl.css('overflow-y', 'scroll');
+                // plugin.topPanelEl.css('padding-right', scrollSettings.width);
+                plugin.htmlEl.addClass('popup-opened');
+            };
+
+            plugin.closePopup = function (popupName) {
+                plugin.reachPopups.filter('[data-popup="' + popupName + '"]').removeClass('opened');
+                setTimeout(function () {
+                    plugin.bodyEl.removeAttr('style');
+                    plugin.htmlEl.removeClass('popup-opened');
+                    plugin.topPanelEl.removeAttr('style');
+                }, 300);
+            };
+
+            plugin.changePopup = function (closingPopup, openingPopup) {
+                plugin.reachPopups.filter('[data-popup="' + closingPopup + '"]').removeClass('opened');
+                plugin.reachPopups.filter('[data-popup="' + openingPopup + '"]').addClass('opened');
+            };
+
+            plugin.init = function () {
+                plugin.bindings();
+            };
+
+            plugin.bindings = function () {
+                plugin.openPopupEl.on('click', function (e) {
+                    e.preventDefault();
+                    var pop = $(this).attr('data-popup-target');
+                    plugin.openPopup(pop);
+                });
+
+                plugin.closePopupEl.on('click', function (e) {
+                    var pop = void 0;
+                    if (this.hasAttribute('data-popup-target')) {
+                        pop = $(this).attr('data-popup-target');
+                    } else {
+                        pop = $(this).closest(options.reachElementClass).attr('data-popup');
+                    }
+
+                    plugin.closePopup(pop);
+                });
+
+                plugin.changePopupEl.on('click', function (e) {
+                    var closingPop = $(this).attr('data-closing-popup');
+                    var openingPop = $(this).attr('data-opening-popup');
+
+                    plugin.changePopup(closingPop, openingPop);
+                });
+
+                plugin.reachPopups.on('click', function (e) {
+                    var target = $(e.target);
+                    var className = options.reachElementClass.replace('.', '');
+                    if (target.hasClass(className)) {
+                        plugin.closePopup($(e.target).attr('data-popup'));
+                    }
+                });
+            };
+
+            if (options) plugin.init();
+
+            return plugin;
+        }
+    }]);
+
+    return YOURAPPNAME;
+}();
+
+(function () {
 
     var app = new YOURAPPNAME(document);
 
@@ -260,5 +286,4 @@
         // App was fully load! Paste external app source code here... 4example if your use jQuery and something else
         // Please do not use jQuery ready state function to avoid mass calling document event trigger!
     });
-
 })();
